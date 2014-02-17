dcaa969f79d91ac714aa7ad599100866d3f94a36
Lifegnard <math.bertoia@gmail.com>
2014-02-17 14:20:08 +0100

    Premier commit

---
+++ src/main/java/dcll/mber/MySimpleStack/App.java
@@ -0,0 +1,13 @@
+package dcll.mber.MySimpleStack;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
